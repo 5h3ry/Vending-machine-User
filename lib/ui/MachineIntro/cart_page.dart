61abad2c9115583955b13039b4d486c2132b5769
5h3ry <5h3ry0021@gmail.com>
2024-02-22 17:28:07 +0500

    4 commit

--- lib/ui/MachineIntro/cart_page.dart
+++ lib/ui/MachineIntro/cart_page.dart
@@ -639,6 +639,9 @@ class _CartPageState extends State<CartPage> {
 
  */
 
+//commit
+
+/*
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -767,6 +770,168 @@ class _CartPageState extends State<CartPage> {
                   child: Text(
                     'Total Bill: \$${totalBill.toStringAsFixed(2)}',
                     style: TextStyle(fontSize: 20), // Adjust font size if needed
+                  ),
+                ),
+              ],
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+ */
+
+
+
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class CartPage extends StatefulWidget {
+  final List<String> selectedIds;
+  final String machineId;
+
+  CartPage({required this.selectedIds, required this.machineId});
+
+  @override
+  _CartPageState createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+  late Future<List<Map<String, dynamic>>> _selectedItemsFuture;
+  late SharedPreferences _prefs; // Add SharedPreferences instance
+
+  Map<String, String> itemQuantities = {};
+  double totalBill = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedItemsFuture = _fetchSelectedItems();
+    _initSharedPreferences(); // Initialize SharedPreferences
+  }
+
+  // Method to initialize SharedPreferences
+  _initSharedPreferences() async {
+    _prefs = await SharedPreferences.getInstance();
+    _loadItemQuantities(); // Load item quantities from SharedPreferences
+  }
+
+  // Method to load item quantities from SharedPreferences
+  _loadItemQuantities() {
+    for (String id in widget.selectedIds) {
+      String? quantity = _prefs.getString(id);
+      if (quantity != null) {
+        itemQuantities[id] = quantity;
+      } else {
+        itemQuantities[id] = '1'; // Set default quantity if not found
+      }
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> _fetchSelectedItems() async {
+    List<Map<String, dynamic>> selectedItemsData = [];
+    await Future.forEach(widget.selectedIds, (String id) async {
+      DocumentSnapshot snapshot = await FirebaseFirestore.instance
+          .collection('Machines')
+          .doc(widget.machineId)
+          .collection('items')
+          .doc(id)
+          .get();
+      if (snapshot.exists) {
+        Map<String, dynamic> data = snapshot.data() as Map<String, dynamic>;
+        selectedItemsData.add(data);
+      }
+    });
+    return selectedItemsData;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFFFFCC00),
+        title: Text('Cart'),
+      ),
+      body: FutureBuilder<List<Map<String, dynamic>>>(
+        future: _selectedItemsFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('No items found in the cart'));
+          } else {
+            totalBill = 0.0;
+            for (int index = 0; index < snapshot.data!.length; index++) {
+              Map<String, dynamic> itemData = snapshot.data![index];
+              String itemId = widget.selectedIds[index];
+              int quantity = int.parse(itemQuantities[itemId] ?? '1');
+              double price = double.parse(itemData['price'].toString());
+              double itemTotal = price * quantity;
+              totalBill += itemTotal;
+            }
+            return Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                       Map<String, dynamic> itemData = snapshot.data![index];
+                       String itemId = widget.selectedIds[index];
+                       int quantity = int.parse(itemQuantities[itemId] ?? '1');
+                      // double price = double.parse(itemData['price'].toString());
+                      // double itemTotal = price * quantity;
+                      // totalBill += itemTotal;
+                      return ListTile(
+                        title: Text(itemData['itemName']),
+                        subtitle: Row(
+                          children: <Widget>[
+                            Text('Price: ${itemData['price']}'),
+                            SizedBox(width: 20),
+                            Text('Quantity: ${itemData['quantity']}'),
+                            SizedBox(width: 20),
+                            IconButton(
+                              icon: Icon(Icons.remove),
+                              onPressed: () {
+                                setState(() {
+                                  if (quantity > 1) {
+                                    quantity--;
+                                    itemQuantities[itemId] = quantity.toString();
+                                    _prefs.setString(itemId, quantity.toString()); // Update quantity in SharedPreferences
+                                  }
+                                });
+                              },
+                            ),
+                            Text('$quantity'),
+                            IconButton(
+                              icon: Icon(Icons.add),
+                              onPressed: () {
+                                setState(() {
+                                  if (quantity < int.parse(itemData['quantity'])) {
+                                    quantity++;
+                                    itemQuantities[itemId] = quantity.toString();
+                                    _prefs.setString(itemId, quantity.toString()); // Update quantity in SharedPreferences
+                                  }
+                                });
+                              },
+                            ),
+
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    'Total Bill: \$${totalBill.toStringAsFixed(2)}',
+                    style: TextStyle(fontSize: 20),
                   ),
                 ),
               ],
